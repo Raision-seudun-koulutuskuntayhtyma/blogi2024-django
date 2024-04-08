6577549c5ddd801f4dac4585d448ca577ba5aed6
Tuomas Suutari <tuomas@nepnep.net>
2024-04-08 13:14:28 +0300

    Formatoi kaikki tiedostot ruff:lla

--- sivusto/wsgi.py
+++ sivusto/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sivusto.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sivusto.settings")
 
 application = get_wsgi_application()
